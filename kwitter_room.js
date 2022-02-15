3ef6433cf007ad78c28901eeaeaef8047de8fd01
Tanmay Aseri <87698651+Tanmay-Aseri@users.noreply.github.com>
2022-02-15 16:11:18 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAxrpbOnEezkn91WtEg8CJXpxCNwxB8kC0",
+  authDomain: "kwitter-project-286bf.firebaseapp.com",
+  databaseURL: "https://kwitter-project-286bf-default-rtdb.firebaseio.com",
+  projectId: "kwitter-project-286bf",
+  storageBucket: "kwitter-project-286bf.appspot.com",
+  messagingSenderId: "1093890736859",
+  appId: "1:1093890736859:web:cef3e38bac4200f0dc82fe"
+};
+const app = initializeApp(firebaseConfig);
